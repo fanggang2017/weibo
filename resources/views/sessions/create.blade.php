f9613ed30d50d1aee03a89bacd8873f1976d8fc3
fanggang <13062892692@163.com>
2020-01-07 09:33:08 +0000

    创建会话，完成登录验证

---
+++ resources/views/sessions/create.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.default')
+@section('title', 'Login')
+
+@section('content')
+    <div class="offset-md-2 col-md-8">
+        <div class="card">
+            <div class="card-header">
+                <h5>Login</h5>
+            </div>
+            <div class="card-body">
+                @include('shared._errors')
+
+                <form action="{{ route('login') }}" method="post">
+                    {{ csrf_field() }}
+
+                    <div class="form-group">
+                        <label for="email">Email:</label>
+                        <input type="text" name="email" class="form-control" value="{{ old('email') }}">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="password">Password:</label>
+                        <input type="password" name="password" id="" class="form-control" value="{{ old('password') }}">
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">Login</button>
+                </form>
+
+                <hr>
+
+                <p>No account yet? <a href="{{ route('signup') }}">Register now!</a></p>
+
+            </div>
+        </div>
+    </div>
+@stop
