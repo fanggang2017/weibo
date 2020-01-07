85fd7b7292d090f3e7f6347ee0aa4a156254c743
fanggang <13062892692@163.com>
2020-01-07 06:11:25 +0000

    用户注册页面

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -2,5 +2,37 @@
 @section('title', 'Register')
 
 @section('content')
-    <h1>Register</h1>
+    <div class="offset-md-2 col-md-8">
+        <div class="card">
+            <div class="card-header">
+                <h5>Register</h5>
+            </div>
+            <div class="card-body">
+                <form action="{{ route('users.store') }}" method="POST">
+                    <div class="form-group">
+                        <label for="name">Name:</label>
+                        <input type="text" name="name" id="" class="form-control" value="{{ old('name') }}">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="email">Email:</label>
+                        <input type="text" name="email" id="" class="form-control" value="{{ old('email') }}">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="password">Password:</label>
+                        <input type="password" name="password" id="" class="form-control" value="{{ old('password') }}">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="password_confirmation">Confirm Password:</label>
+                        <input type="password" name="password_confirmation" id="" class="form-control" value="{{ old('password_confirmation') }}">
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">Register</button>
+
+                </form>
+            </div>
+        </div>
+    </div>
 @stop
